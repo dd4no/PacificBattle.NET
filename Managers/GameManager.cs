0b936b2cb157f1970cd1db9f20f5860b5135152a
Dave Defourneaux <74273226+dd4no@users.noreply.github.com>
2025-02-02 17:02:09 -0500

    Add general Manager structure

---
+++ Managers/GameManager.cs
@@ -0,0 +1,38 @@
+﻿
+using PacificBattle.Models;
+
+namespace PacificBattle.Managers
+{
+    public class GameManager
+    {
+        private FleetManager fleetManager;
+        private TurnManager turnManager;
+        private SeaManager seaManager;
+
+        public int Turn { get; private set; }
+        public int POC { get; private set; }
+        public string WinningSide { get; private set; } = string.Empty;
+        public List<CombatShip> ActiveShips { get; private set; } = [];
+
+        public GameManager(FleetManager fm, TurnManager tm, SeaManager sm)
+        {
+            fleetManager = fm;
+            turnManager = tm;
+            seaManager = sm;
+        }
+
+        public void StartGame()
+        {
+            Turn = 1;
+            ActiveShips.Clear();
+            ActiveShips = fleetManager.ActiveShips;
+            POC = 0;
+            WinningSide = string.Empty;
+            turnManager.StartTurnOne();
+        }
+
+        // initiate turn change
+        // keep track of score
+        // monitor end game condition
+    }
+}
---
+++ Managers/TurnManager.cs
@@ -0,0 +1,32 @@
+﻿namespace PacificBattle.Managers
+{
+    public class TurnManager
+    {
+        private GameManager gm;
+        private FleetManager fleetManager;
+
+        public int Turn { get; private set; } = 1;
+        public int POC { get; }
+
+        public TurnManager(GameManager gmgr, FleetManager fmgr)
+        {
+            gm = gmgr;
+            fleetManager = fmgr;
+        }
+
+        public void StartTurnOne()
+        {
+            fleetManager.ChangeTurns(1);
+        }
+
+        private void StartNewTurn()
+        {
+            Turn++;
+            fleetManager.ChangeTurns(Turn);
+        }
+
+        // activate/deactivate ships
+        // tally poc
+
+    }
+}
